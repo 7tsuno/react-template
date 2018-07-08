4f3eb927b7e34e2aaaad03ce80fcb74efc8b8902
natsuno-sino <natsuno.sino@gmail.com>
2018-07-08 15:32:01 +0900

    first commit

---
+++ assets/js/components/Application.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Application extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello React
+      </div>
+    );
+  }
+}
+
+export default Application;
